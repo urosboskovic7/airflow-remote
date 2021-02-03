5ad0ad6b4d7bb036ebcfef8b32b8b6435333fb6f
Uros Boskovic <uros.boskovic@eng.it>
2021-02-03 19:11:56 +0100

    Add new dag

--- dags/delete-dags.py
+++ dags/delete-dags.py
@@ -55,7 +55,7 @@ default_args = {
 }
 
 dag = DAG(
-    'tutorial',
+    'delete_dags',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
@@ -63,42 +63,10 @@ dag = DAG(
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
+    task_id='delete_dags',
+    bash_command='rm /opt/airflow/dags',
     dag=dag,
 )
 
-t1.doc_md = """\
-#### Task Documentation
-You can document your task using the attributes `doc_md` (markdown),
-`doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-rendered in the UI's Task Instance Details page.
-![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-"""
 
-dag.doc_md = __doc__
-
-t2 = BashOperator(
-    task_id='sleep',
-    depends_on_past=False,
-    bash_command='sleep 5',
-    dag=dag,
-)
-
-templated_command = """
-{% for i in range(5) %}
-    echo "{{ ds }}"
-    echo "{{ macros.ds_add(ds, 7)}}"
-    echo "{{ params.my_param }}"
-{% endfor %}
-"""
-
-t3 = BashOperator(
-    task_id='templated',
-    depends_on_past=False,
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag,
-)
-
-t1 >> [t2, t3]
+t1
